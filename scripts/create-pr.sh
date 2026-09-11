36e68c6927e440d5748e212507805ac707417503
Sam Doran <sdoran@redhat.com>
2026-09-10 21:04:26 -0400

    Make the branch name unique and add a refspec (#28)
    A refspec is required without additional settings.

--- scripts/create-pr.sh
+++ scripts/create-pr.sh
@@ -7,9 +7,9 @@ if [[ -z $(git status --porcelain -- Containerfile) ]]; then
     exit 0
 fi
 
-git switch -c update-base-image
+git switch -c "update-base-image-$(date +%s)"
 git commit -i Containerfile -m "Update base image"
-git push "$(git remote | head -n 1)"
+git push "$(git remote | head -n 1)" HEAD
 
 PR_URL="$(gh pr create --title 'Update base image' --body '' | tail -n 1)" || exit $?
 
