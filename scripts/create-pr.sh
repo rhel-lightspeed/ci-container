2e130278cfafa50e55909879417d9bab4ab25f80
Sam Doran <sdoran@redhat.com>
2026-09-10 21:25:05 -0400

    Specify refspec (#30)

--- scripts/create-pr.sh
+++ scripts/create-pr.sh
@@ -10,7 +10,7 @@ fi
 git switch -c "update-base-image-$(date +%s)"
 git commit -i Containerfile -m "Update base image"
 git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-git push origin
+git push origin HEAD
 
 PR_URL="$(gh pr create --title 'Update base image' --body '' | tail -n 1)" || exit $?
 gh pr merge --auto --squash "$PR_URL"
